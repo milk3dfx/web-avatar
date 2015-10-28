d0cf6d60360f9cddc4e791f9ea903f8ea3d3ea4a
Sergey Zolotykh <Sergey Zolotykh>
2015-10-27 22:25:14 -0400

    Initial commit

---
+++ dialogs/dialog-1.js
@@ -0,0 +1,21 @@
+{
+"root":"1",
+"questions":
+	[
+	{
+	"id": "1",
+	"question" : "Hello! How are you?",
+	"answers" :
+		[
+			{
+			"answer": "Good",
+			"action": "2"
+			},
+			{
+			"answer": "Good",
+			"action": "3"
+			}
+		]
+	}
+	]	
+}
